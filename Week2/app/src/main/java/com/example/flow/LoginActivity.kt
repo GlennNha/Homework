f30836fc740e792118aecf73cd5b2d107dab5083
GlennNha <nha8266174@gmail.com>
2021-04-14 19:37:51 +0700

    Nha: DataBinding

--- Week2/app/src/main/java/com/example/flow/LoginActivity.kt
+++ Week2/app/src/main/java/com/example/flow/LoginActivity.kt
@@ -1,35 +1,31 @@
 package com.example.flow
 
 import android.content.Intent
+import android.database.DatabaseUtils
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.flow.databinding.LoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : LoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
         supportActionBar?.hide()
-        val btnLogIn = findViewById<Button>(R.id.buttonLogIn)
-        btnLogIn.setOnClickListener{
-            var inputPass = findViewById<EditText>(R.id.inputPassword).text.toString()
-            var inputEmail = findViewById<EditText>(R.id.inputEmail).text.toString()
-            if(inputPass == "123456" && inputEmail == "ronaldo@gmail.com"){
+        binding = DataBindingUtil.setContentView(this, R.layout.login)
+
+        binding.buttonLogIn.setOnClickListener{
+            if(binding.inputPassword.text.toString() == "123456" && binding.inputEmail.text.toString() == "ronaldo@gmail.com"){
                 Toast.makeText( this@LoginActivity, "Login Successfully", Toast.LENGTH_SHORT).show()
-                /*Switch màn hình
-                    thay ProfileActivity bằng màn hình Profile
-                    val intent = Intent( this@MainActivity, ProfileActivity::class.java)
-                    startActivity(intent)
-                 */
                 val intent = Intent(this, Activity_Profile_ViTien::class.java)
                 startActivity(intent)
             }else{
                 Toast.makeText( this@LoginActivity, "Login Failed! Please try again.", Toast.LENGTH_SHORT).show()
             }
         }
-
     }
 
     override fun onStart() {
